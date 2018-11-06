ec7f5aae555fe7c6ada173dc62dafa32a34867c7
Allan Huang <allan.huang@recreo.com.au>
2018-11-06 17:27:32 +1100

    add node scripts

---
+++ NodeScripts/GenCrtbl.js
@@ -0,0 +1,27 @@
+var sprintf = require("sprintf-js");
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./crtblTest02.txt', { autoClose: true })
+});
+var isNewTable = true;
+var isEndOfTable = false;
+
+lineReader.on('line', function (line) {
+    var arr = line.split("\t");
+
+    if (arr[1] == "DataType") {
+        console.log('Create Table ', arr[0], ' (');
+        isNewTable = true;
+        isEndOfTable = false;
+    }
+    else if (arr[1] == undefined) {
+        if(!isEndOfTable)
+        {
+            console.log(')\n', 'go\n');
+            isEndOfTable = true;
+        }
+    }
+    else {
+        console.log(sprintf.sprintf("%1s %-36s %-20s %s", (isNewTable)?" ":",", arr[0] ,arr[1], ((arr[2]=="Y")?"":"Not Null")));
+        isNewTable = false;
+    }
+});
